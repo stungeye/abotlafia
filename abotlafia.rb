de5d4c77cab9d434301e6577128adcb60c28a5c7
Wally Glutton <stungeye@gmail.com>
2012-01-08 00:11:24 -0600

    A little clean up.

--- abotlafia.rb
+++ abotlafia.rb
@@ -1,5 +1,5 @@
 # encoding: UTF-8
-not_now = rand(10) > 4
+not_now = rand(10) > 3
 exit if not_now
 
 require 'rubygems'
@@ -7,27 +7,8 @@ require 'chatterbot/dsl'
 require "sequel"
 require "./db_connect.rb"
 
-# remove this to send out tweets
 # debug_mode
-
-# remove this to update the db
 no_update
-# remove this to get less output when running
-# verbose
-
-# here's a list of users to ignore
-# blacklist "abc", "def"
-
-# here's a list of things to exclude from searches
-# exclude "hi", "spammer", "junk"
-
-# search "keyword" do |tweet|
-#  reply "Hey #USER# nice to meet you!", tweet
-# end
-
-# replies do |tweet|
-#   reply "Yes #USER#, you are very kind to say that!", tweet
-# end
 
 DB = Sequel.connect("mysql2://#{DB_USER}:#{DB_PASSWORD}@localhost/stungeye")
 random_sentence = DB.fetch("SELECT * FROM `sentences` WHERE `tweeted` = FALSE ORDER BY RAND() LIMIT 1").first
